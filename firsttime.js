988d4101f616c861bf58550384709c8287b7c3c9
DavidGamingRoblox <60021100+DavidGamingRoblox@users.noreply.github.com>
2023-05-27 09:28:29 -0400

    ready to go

---
+++ firsttime.js
@@ -0,0 +1,17 @@
+function firstTimeInit() {
+    openModal('welcome-modal');
+}
+
+function firstTimeSetupLoadDefaults() {
+    $('#welcome-setup-username').val('cvmPlusPlus' + (Math.floor(Math.random() * 8999) + 1000).toString());
+    $('#welcome-setup-server-list').val(`aceofwalls.chickenkiller.com/cvm`);
+}
+
+function firstTimeSetupSaveSettings() {
+    localStorage.setItem('username', $('#welcome-setup-username').val());
+    localStorage.setItem('servers', JSON.stringify($('#welcome-setup-server-list').val().split('\n')));
+    localStorage.setItem('chatSound', 'http://computernewb.com/collab-vm/notify.ogg')
+    localStorage.setItem('showChatTime', JSON.stringify(true));
+    localStorage.setItem('showChatTimeInUTC', JSON.stringify(true));
+    localStorage.setItem('vmViewFit', JSON.stringify(false));
+}
